453a07b6d3b9484925b0b8fb6f056ccad719d249
Shyam Joshi <sjuicyy@gmail.com>
2023-01-26 17:16:01 +0545

    revision

--- db.py
+++ db.py
@@ -1,27 +1,26 @@
-from basic import db ,Puppy
+from basic import db, Puppy
 
 
 data=Puppy.query.all()
 print(data)
 
-
-data=Puppy.query.get(1)
+data= Puppy.query.get(1)
 print(data.name)
 
-data=Puppy.query.filter_by(name='ashlin')
+data = Puppy.query.filter_by(name='jackie')
 print(data.all())
 
-
-
 data=Puppy.query.get(1)
-data.name='ashlin'
+data.name='asline'
 db.session.add(data)
 db.session.commit()
 
-
 data=Puppy.query.get(1)
 print(data.name)
 
-data=Puppy.query.get(4)
+data= Puppy.query.all()
+print(data)
+
+data=Puppy.query.get(3)
 db.session.delete(data)
 db.session.commit()
